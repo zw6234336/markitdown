f20c964f990fbdf14408e8f3876b68b3f2ea8eb0
Adam Fourney <adamfo@microsoft.com>
2024-11-13 13:00:01 -0800

    Initial commit.

---
+++ src/markitdown/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024-present Adam Fourney <adamfo@microsoft.com>
+#
+# SPDX-License-Identifier: MIT
+
+from ._markitdown import MarkItDown
+
+__all__ = [
+    "MarkItDown",
+]
